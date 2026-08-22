818d0cb034e932680fe84c4a313f98d3a818481d
Caleb <caleb.wu9@gmail.com>
2026-08-22 15:15:57 +1000

    autoscaling

--- main.tf
+++ main.tf
@@ -32,25 +32,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id              = module.blog_vpc.public_subnets[0]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y tomcat10
-              systemctl start tomcat10
-              systemctl enable tomcat10
-              EOF
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -101,4 +83,26 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_group_arn = aws_lb_target_group.blog.arn
   target_id        = aws_instance.blog.id
   port             = 80
+}
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.2"
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+
+  launch_template_name = "blog"
+  security_groups = [module.blog_sg.security_group_id]
+  instance_type          = var.instance_type
+  image_id                    = data.aws_ami.ubuntu.id
+
+  traffic_source_attachments = {
+    blog-alb = {
+      traffic_source_identifier = aws_lb_target_group.blog.arn
+    }
+  }
 }
