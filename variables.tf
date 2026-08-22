73a1053cd123d41632b0966aaf08f79b0c98410d
Caleb <caleb.wu9@gmail.com>
2026-08-22 15:29:02 +1000

    refactor to use variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,39 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+  type = object ({
+    name = string
+    owner = string
+  })
+  default {
+    name = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
+    owner = "099720109477" # Official Canonical account ID
+  }
+}
+
+
+variable "environment" {
+  description = "Deployment environment"
+  type = object ({
+    name = string
+    network_prefix = string
+  })
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+
+variable "min_size" {
+  description = "Minimum number of instances in the ASG"
+  default = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in the ASG"
+  default = 2
+}
