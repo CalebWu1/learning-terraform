9727be24df7f2eb92f3d7fb274c4b775d21c9956
Caleb <caleb.wu9@gmail.com>
2026-08-22 14:36:52 +1000

    Reverting module to v5

--- main.tf
+++ main.tf
@@ -39,10 +39,10 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "6.0.0"
-  name    = "blog_new"
-
-  vpc_id = data.aws_vpc.default.id
+  version = "~> 5.0" 
+  
+  name    = "blog_new" 
+  vpc_id  = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
